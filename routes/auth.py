e6004bfa9cee831ac356bbcfcbf3397883422c6c
hebressus@gmail.com <josemi@debian-BULLSEYE-live-builder-AMD64>
2022-12-07 13:23:46 -0300

    Initial

---
+++ routes/auth.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from pydantic import BaseModel, EmailStr
+
+auth_routes = APIRouter()
+
+class User(BaseModel):
+    username = str,
+    email: EmailStr
+
+@auth_routes.post("/login")
+def login(user: User):
+    print(user)
+    return "success"
